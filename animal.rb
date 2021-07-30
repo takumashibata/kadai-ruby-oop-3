a30baa916617dd35ff57c8641d0c0d377ca459e8
takumashibata <westside_storydoor@yahoo.co.jp>
2021-07-30 18:29:07 +0000

    first commit

---
+++ animal.rb
@@ -0,0 +1,14 @@
+class Animal
+    
+    attr_accessor :name, :age
+    
+    def initialize(name,age)
+        @name = name
+        @age = age
+    end
+    
+    def say
+        puts "#{@name}です。#{@age}歳です。"
+    end
+    
+end
